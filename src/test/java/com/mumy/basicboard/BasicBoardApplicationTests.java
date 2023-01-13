2fa2ec7dc404468868d124b9ad764129d2f4f359
Dukapuri <vivian991031@naver.com>
2023-01-13 20:01:34 +0900

    파일 구조 수정
    파일 구조 수정

---
+++ src/test/java/com/mumy/basicboard/BasicBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mumy.basicboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
