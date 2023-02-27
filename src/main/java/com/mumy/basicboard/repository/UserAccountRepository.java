e82d5c2e8558ae50a4eb33e89648de732c4c416a
황성태 <vivian991031@naver.com>
2023-02-27 14:38:18 +0900

    #22 - Repository 업데이트 및 생성
    ArticleCommentRepository에 Article의 ID를 가지고 관련된 댓글들을 찾을 수 있는 findByArticle_Id함수 추가 
    ArticleRepository에서 pageable을 넘겨주어 Article의 페이징을 가져올 수 있는 함수 findByTitle생성
    UserAccountRepository 생성

---
+++ src/main/java/com/mumy/basicboard/repository/UserAccountRepository.java
@@ -0,0 +1,7 @@
+package com.mumy.basicboard.repository;
+
+import com.mumy.basicboard.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+}
